2be92b770a8bbef457402bde5438f831b562956e
snu08026 <snu08026@users.noreply.github.com>
2026-07-02 14:43:40 +0900

    Add PWA support

---
+++ sw.js
@@ -0,0 +1,37 @@
+const CACHE_NAME = 'refresh-helper-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './coupang_logo.png',
+  './our_logo.png'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((cachedResponse) => {
+      return cachedResponse || fetch(e.request);
+    })
+  );
+});
